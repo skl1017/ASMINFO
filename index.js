6d5cb983e4eec514e3b146bcd317140ef3ca72c3
skl1017 <113782293+skl1017@users.noreply.github.com>
2023-06-30 15:07:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 window.onload=(event) =>{
-    const header = document.querySelector('header')
+    const header = document.querySelector('header');
+    
+
+    window.addEventListener('scroll', (scroll) =>{
+        y = scrollY;
+        if(y > 130){
+            header.classList.add('black');
+        }
+        else{
+            if(header.classList.contains('index-bar')){
+                header.classList.remove('black')
+            }
+        }
+    })
+
 
 
 
